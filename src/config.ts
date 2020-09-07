f2d979f33971e6d729f5b1c9ceb6030eed5de16c
Alonzo Patterson <zo@emptyblocks.com>
2020-09-07 19:24:13 -0400

    updating msal creds

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    appId: 'd97c17ed-c7e3-4e92-92c8-85dfb98148f4',
-    redirectUri: 'https://dlamil.dps.mil/',
+    appId: 'ffc0206c-8bb0-4653-bdaf-c89568cef28e',
+    redirectUri: 'https://dlamil.dps.mil/sites/SPO_PEODashboard',
     scopes: [
       'user.read',
       'calendars.read'
