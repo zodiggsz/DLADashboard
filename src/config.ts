8d6bbc51a7b5c73c95cd036db4737cdc766ca3bc
RayShawn Ware <rayshawnware@RayShawns-MacBook-Pro.local>
2021-07-19 13:41:48 -0400

    removed ecm admins from being able to add peo as a user role. commented out peo to be included as a user role for all user roles.

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const config = {
-    spURi: 'https://dlamil.dps.mil/sites/SPO_PEODashboard',
+    //spURi: 'https://dlamil.dps.mil/sites/SPO_PEODashboard',
     // spURi: 'https://dlamil.dps.mil/sites/STAGE_PEODashboard',
-     //spURi: 'https://codicast1.sharepoint.com/',
+    spURi: 'https://codicast1.sharepoint.com/',
     appId: 'ffc0206c-8bb0-4653-bdaf-c89568cef28e',
     redirectUri: 'https://dlamil.dps.mil/sites/SPO_PEODashboard',
     // redirectUri: 'https://dlamil.dps.mil/sites/STAGE_PEODashboard',
